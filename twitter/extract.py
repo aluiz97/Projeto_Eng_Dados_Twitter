f33bcf8a6068b3a255005dc0476de3edb447f048
aluiz97 <a.luiz97@hotmail.com>
2022-09-30 17:27:18 -0300

    segundo commit

--- twitter/extract.py
+++ twitter/extract.py
@@ -1,13 +1,23 @@
-import tweepy
+# %%
+import tweepy as tw
 from dotenv import load_dotenv
 from os import getenv
-
-load_dotenv('.env')
-api_key = getenv('api_key')
-api_key_secret = getenv('api_key_secret')
-access_token = getenv('acess_token')
-access_token_secret = getenv('acess_token_secret')
-
-stream = tweepy.Stream(api_key, api_key_secret, access_token, access_token_secret)
-
-stream.filter(track=['voto', 'eleicao'])
+#%%
+load_dotenv('/home/antonio-linux/PROJETOS/Projeto_Eng_Dados_Twitter/.env')
+api_key = str(getenv('api_key'))
+api_key_secret = str(getenv('api_key_secret'))
+access_token = str(getenv('access_token'))
+access_token_secret = str(getenv('access_token_secret'))
+#%%
+#stream = tw.Stream(api_key, api_key_secret, access_token, access_token_secret)
+auth = tw.OAuthHandler(api_key, api_key_secret)
+auth.set_access_token(access_token, access_token_secret)
+#%%
+api = tw.Client(auth)
+#public_tweets = api.home_timeline()
+# %%
+public_tweets = api.search_all_tweets(query='voto')
+# %%
+for tweet in public_tweets:
+    print(tweet.text)
+# %%
